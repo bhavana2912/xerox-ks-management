6e6dd2c1e14840ded956be894cb22773283114a1
Bhavana <bhavanasankar2912@gmail.com>
2026-08-07 23:13:04 +0530

    updated varaible

--- firebase_config.py
+++ firebase_config.py
@@ -1,10 +1,14 @@
+import os
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("serviceAccountKey.json")
+firebase_path = os.getenv(
+    "GOOGLE_APPLICATION_CREDENTIALS",
+    "serviceAccountKey.json"
+)
 
 if not firebase_admin._apps:
+    cred = credentials.Certificate(firebase_path)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
